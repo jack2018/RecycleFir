1d013a455351820da09d0df4452499bcf173f80e
she <1253865188@qq.com>
2018-01-28 19:41:20 +0800

    new 分支

--- app/src/main/java/com/example/administrator/recycle/MainActivity.java
+++ app/src/main/java/com/example/administrator/recycle/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArrayList<String> s = new ArrayList<>();//newashdlkas
+        ArrayList<String> s = new ArrayList<>();//newashdl新的主分支
         s.add("ww1");
         s.add("ee2");
         s.add("rr3");
